6db728840627425d5019a2d2273ad80de679b186
Michael Gerzabek <michael.gerzabek@gmail.com>
2022-02-20 08:53:35 +0100

    Release 0.7.0

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
 ## 0.1.0
 
 - Initial Import
+
+## 0.7.0
+
+- Added inline citations
--- lib/jekyll/zettel/version.rb
+++ lib/jekyll/zettel/version.rb
@@ -1,5 +1,5 @@
 module Jekyll
   module Zettel
-    VERSION = '0.6.11'.freeze
+    VERSION = '0.7.0'.freeze
   end
 end
